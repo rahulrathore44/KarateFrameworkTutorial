38d2e2a25dd2cfa572c3a5dceec9ab227b7daed0
rathr1 <rathr1@L33986WUS>
2020-11-27 11:04:10 -0800

    Added the Result object in the runner

--- src/test/java/com/api/automation/ParallelBuilder.java
+++ src/test/java/com/api/automation/ParallelBuilder.java
@@ -1,7 +1,9 @@
 package com.api.automation;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import com.intuit.karate.Results;
 import com.intuit.karate.Runner.Builder;
 
 public class ParallelBuilder {
@@ -11,7 +13,13 @@ public class ParallelBuilder {
 		Builder aRunner = new Builder();
 		//aRunner.path("classpath:com/api/automation/getrequest");
 		aRunner.path("classpath:com/api/automation");
-		aRunner.parallel(5);
+		// aRunner.parallel(5);
+		Results result = aRunner.parallel(5);
+		System.out.println("Total Feature => " + result.getFeatureCount());
+		System.out.println("Total Scenarios => " + result.getScenarioCount());
+		System.out.println("Passed Scenarios => " + result.getPassCount());
+		
+		Assertions.assertEquals(0, result.getFailCount(), "There are Some Failed Scenarios ");
 		
 	}
 
