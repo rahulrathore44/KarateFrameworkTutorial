55f36a535590f284d83e30ba7e9ea31428a9e845
rathr1 <rathr1@L33986WUS>
2020-12-06 12:51:47 -0800

    Update the implementation

--- src/test/java/com/api/automation/ParallelRunnerWithTags.java
+++ src/test/java/com/api/automation/ParallelRunnerWithTags.java
@@ -24,37 +24,44 @@ public class ParallelRunnerWithTags {
 		// Runner.parallel(aRunner);
 	}
 
-	// Provide the values for location and tags and separate them with ","
-	// Use the "," to split the string and create List out of it
-		
-	/*
-	 * private List<String> getTags() { String aTags = System.getProperty("tags",
-	 * "@Confidence"); List<String> aTagList = Arrays.asList(aTags); return
-	 * aTagList; }
-	 * 
-	 * private List<String> getLocation() { String aLocation =
-	 * System.getProperty("location", "com/api/automation"); List<String>
-	 * aLocationList = Arrays.asList(CLASS_PATH + aLocation); return aLocationList;
-	 * }
-	 */
+	// Step 1 - Provide the values for location and tags property. All the values will be separate by ","
+	// Read the values , split them using the "," and create a list out of it.
+
+	/*private List<String> getTags() {
+		String aTags = System.getProperty("tags", "@Confidence");
+		List<String> aTagList = Arrays.asList(aTags);
+		return aTagList;
+	}
+
+	private List<String> getLocation() {
+		String aLocation = System.getProperty("location", "com/api/automation");
+		List<String> aLocationList = Arrays.asList(CLASS_PATH + aLocation);
+		return aLocationList;
+	} */
+	
 	private List<String> getTags() {
-		String aTags = System.getProperty("tags","@Confidence");
+		String aTags = System.getProperty("tags", "@Confidence");
 		List<String> aTagList = Collections.emptyList();
+		// First check for the delimiter (,)
+		// if the aTags has the delimiter then split the string using the delimiter 
+		// And create the list out of it
+		// if aTags does not have delimiter, the use the old logic
 		if(aTags.contains(DELIMITER)) {
-			String aTagArray[] = aTags.split(DELIMITER);
-			aTagList = Arrays.asList(aTagArray);
+			String tagArray[] = aTags.split(DELIMITER);
+			aTagList = Arrays.asList(tagArray);
 			return aTagList;
 		}
 		aTagList = Arrays.asList(aTags);
 		return aTagList;
+		
 	}
 	
 	private List<String> getLocation() {
 		String aLocation = System.getProperty("location", "com/api/automation");
 		List<String> aLocationList = Collections.emptyList();
 		if(aLocation.contains(DELIMITER)) {
-			String aLocationArray[] = aLocation.split(DELIMITER);
-			aLocationList = Arrays.asList(aLocationArray);
+			String locationArray[] = aLocation.split(DELIMITER);
+			aLocationList = Arrays.asList(locationArray);	
 			aLocationList.replaceAll((entry) -> {
 				return CLASS_PATH + entry;
 			});
@@ -63,5 +70,5 @@ public class ParallelRunnerWithTags {
 		aLocationList = Arrays.asList(CLASS_PATH + aLocation);
 		return aLocationList;
 	}
-	
+
 }
