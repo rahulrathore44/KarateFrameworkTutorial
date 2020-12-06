4d6119029395c9c6e46e748fe251b9b4d722a9c9
rathr1 <rathr1@L33986WUS>
2020-12-05 19:28:30 -0800

    Added the parallel runner

--- src/test/java/com/api/automation/ParallelRunnerWithTags.java
+++ src/test/java/com/api/automation/ParallelRunnerWithTags.java
@@ -1,6 +1,7 @@
 package com.api.automation;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,7 @@ import com.intuit.karate.Runner.Builder;
 public class ParallelRunnerWithTags {
 
 	private static final String CLASS_PATH = "classpath:";
+	private static final String DELIMITER = ",";
 
 	@Test
 	public void executeKarateTests() {
@@ -22,15 +24,44 @@ public class ParallelRunnerWithTags {
 		// Runner.parallel(aRunner);
 	}
 
+	// Provide the values for location and tags and separate them with ","
+	// Use the "," to split the string and create List out of it
+		
+	/*
+	 * private List<String> getTags() { String aTags = System.getProperty("tags",
+	 * "@Confidence"); List<String> aTagList = Arrays.asList(aTags); return
+	 * aTagList; }
+	 * 
+	 * private List<String> getLocation() { String aLocation =
+	 * System.getProperty("location", "com/api/automation"); List<String>
+	 * aLocationList = Arrays.asList(CLASS_PATH + aLocation); return aLocationList;
+	 * }
+	 */
 	private List<String> getTags() {
-		String aTags = System.getProperty("tags", "@Confidence");
-		List<String> aTagList = Arrays.asList(aTags);
+		String aTags = System.getProperty("tags","@Confidence");
+		List<String> aTagList = Collections.emptyList();
+		if(aTags.contains(DELIMITER)) {
+			String aTagArray[] = aTags.split(DELIMITER);
+			aTagList = Arrays.asList(aTagArray);
+			return aTagList;
+		}
+		aTagList = Arrays.asList(aTags);
 		return aTagList;
 	}
-
+	
 	private List<String> getLocation() {
 		String aLocation = System.getProperty("location", "com/api/automation");
-		List<String> aLocationList = Arrays.asList(CLASS_PATH + aLocation);
+		List<String> aLocationList = Collections.emptyList();
+		if(aLocation.contains(DELIMITER)) {
+			String aLocationArray[] = aLocation.split(DELIMITER);
+			aLocationList = Arrays.asList(aLocationArray);
+			aLocationList.replaceAll((entry) -> {
+				return CLASS_PATH + entry;
+			});
+			return aLocationList;
+		}
+		aLocationList = Arrays.asList(CLASS_PATH + aLocation);
 		return aLocationList;
 	}
+	
 }
