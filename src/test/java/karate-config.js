89e1a5192087dd50872f0affba942c9772cdbc56
rahulrathore44 <rathr1@L33986WUS>
2020-12-23 18:42:15 -0800

    Config for env

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -2,7 +2,7 @@ function fn() {
   var env = karate.env; // get system property 'karate.env'
   karate.log('karate.env system property was:', env);
   if (!env) {
-    env = 'staging';
+    env = 'stating';
   }
   var config = {
     env: env,
@@ -20,7 +20,22 @@ function fn() {
   } else if (env == 'e2e') {
 	  config.username = 'user';
 	  config.password = 'userpassword';
-  }
+  } else if (env == 'staging') {
+	  // Initialize the config for staging
+	  config.username = 'stagingadmin1';
+	  config.password = 'stagingwelcome';
+	  config._url= 'http://staging.localhost:9898';
+  } else if (env == 'preprod') {
+	  // Initialize the config for preprod
+	  config.username = 'preprodadmin1';
+	  config.password = 'preprodwelcome';
+	  config._url= 'http://preprod.localhost:9898';
+  } else if (env == 'prod') {
+	  // Initialize the config for prod
+	  config.username = 'prodadmin1';
+	  config.password = 'prodwelcome';
+	  config._url= 'http://localhost:9898';
+  } 
   
   return config;
 }
